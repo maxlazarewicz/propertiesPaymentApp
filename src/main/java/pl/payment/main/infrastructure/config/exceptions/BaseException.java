01063774f7f9d34395f2d97bca68a4d03fd20233
maxlazarewicz <juniorjavamlazarewicz@gmail.com>
2023-01-25 17:22:42 +0100

    Base exception extends RunTimeException

--- src/main/java/pl/payment/main/infrastructure/config/exceptions/BaseException.java
+++ src/main/java/pl/payment/main/infrastructure/config/exceptions/BaseException.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import pl.payment.main.infrastructure.config.errorcode.ErrorCode;
 
 @Slf4j
-public class BaseException extends Exception {
+public class BaseException extends RuntimeException {
 
     private ErrorCode errorCode;
 
